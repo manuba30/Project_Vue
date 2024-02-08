9ae15be70a954df6a9379e49156b2844f01b2198
Manoel <manoelcoelho3003@gmail.com>
2024-02-09 00:22:38 +0100

    first commit

---
+++ vue-project/src/views/CounterView.vue
@@ -0,0 +1,13 @@
+<script setup>
+import { useCounterStore } from '@/stores/counter';
+
+let counter = useCounterStore()
+</script>
+
+<template>
+    <div>
+        <h1>{{ counter.count }}</h1>
+
+        <button @click = "counter.count++">increment</button>
+    </div>
+</template>
---
+++ vue-project/src/views/HomeView.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { ref } from 'vue';
+import TheWelcome from '../components/TheWelcome.vue';
+
+let message = ref("hello, world!");
+
+let doSomething =  () => {
+    alert("doing it");
+    }
+</script>
+
+<template>
+  <main>
+    <TheWelcome />
+    <p>{{ message }}</p>
+    <button @click = doSomething>Click me!</button>
+  </main>
+</template>
